43b91f47adde6ca2ce9c1dfb82bd8d63d58cb5d8
wcdmac <wcdmac@hotmail.com>
2026-05-24 21:26:21 +0800

    Add OCTGroup.m

---
+++ Classes/Private/Manager/Objects/OCTGroup.m
@@ -0,0 +1,13 @@
+#import "OCTGroup.h"
+
+@implementation OCTGroup
+
++ (NSString *)primaryKey {
+    return @"uniqueIdentifier";
+}
+
++ (NSArray *)indexedProperties {
+    return @[@"groupNumber"];
+}
+
+@end
