234f1fa06a67ff6383e58383bb24e1865923bcaf
wcdmac <wcdmac@hotmail.com>
2026-05-24 23:54:22 +0800

    Fix import: use framework-style imports in Classes/Public/Manager/OCTGroupConstants.h

--- Classes/Public/Manager/OCTGroupConstants.h
+++ Classes/Public/Manager/OCTGroupConstants.h
@@ -2,8 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#import <Foundation/Foundation.h>
-#import "OCTToxConstants.h"
+iimport <Foundation/Foundation.h>
+iimport "OCTToxConstants.h"
 
 typedef uint32_t OCTGroupNumber;
 typedef uint32_t OCTGroupPeerNumber;
