4c2fa6dd19fdd04240e1c255fcf386740891414f
wcdmac <wcdmac@hotmail.com>
2026-05-25 11:00:57 +0800

    Fix: Add NS_SWIFT_NAME to decodeData to prevent renaming

--- Classes/Public/OpusBridge.h
+++ Classes/Public/OpusBridge.h
@@ -5,7 +5,7 @@
 - (instancetype)initWithSampleRate:(int)sampleRate channels:(int)channels;
 
 - (NSData *)encodePCMBuffer:(const int16_t *)buffer frameCount:(int)frameCount;
-- (int)decodeData:(NSData *)encodedData toPCMBuffer:(int16_t *)buffer frameCount:(int)frameCount;
+- (int)decodeData:(NSData *)encodedData toPCMBuffer:(int16_t *)buffer frameCount:(int)frameCount NS_SWIFT_NAME(decodeData(_:toPCMBuffer:frameCount:));
 
 - (void)destroyEncoder;
 - (void)destroyDecoder;
